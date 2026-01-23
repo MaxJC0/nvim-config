f4f4ba3a1e70502a3a8c18bf00af9b24b993f92b
MaxJCO <mirnikmax@gmail.com>
2026-01-23 20:19:19 +0100

    added tab behaviour and width

--- init.lua
+++ init.lua
@@ -205,6 +205,13 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Configure tab behavior
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.shiftwidth = 2 -- indentation width
+vim.opt.tabstop = 2 -- how tabs are displayed
+vim.opt.softtabstop = 2 -- tab key inserts 2 spaces
+vim.opt.list = false -- hide >> and · markers
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
